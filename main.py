5dae278e2ba101d81bcb54bd7d5e7294fe8e1f57
Spencer Barnes <interstellarjumper@gmail.com>
2020-02-03 19:11:48 -0600

    Create initial python

---
+++ main.py
@@ -0,0 +1,35 @@
+def main():
+  pass
+
+# def runBMI():
+#   pass
+
+# def getHeight():
+#   return double
+
+# def getWeight():
+#   return double
+
+# def calculateBMI(height, weight):
+#   return double
+
+# def runRetirementCalculator():
+#   pass
+
+# def getCurrentAge():
+#   return int
+
+# def getSalary():
+#   return int
+
+# def getPercentSaved():
+#   return double
+
+# def getSaveGoal():
+#   return double
+
+# def calculateRetirementAge(age, salary, savingPercentage, saveGoal):
+#   return int
+
+if (__name__ == "__main__"):
+  main()
