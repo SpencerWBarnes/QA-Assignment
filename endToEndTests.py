223fe81bbac7e263462476974501212fca95cf62
Spencer Barnes <interstellarjumper@gmail.com>
2020-03-18 17:01:39 -0500

    Add end-to-end testing file

---
+++ endToEndTests.py
@@ -0,0 +1,14 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+# # create a new Chrome session
+# driver = webdriver.Chrome()
+# driver.implicitly_wait(30)
+
+# # Navigate to the application home page
+# driver.get("http://www.google.com")
+
+# assert(driver.title == "Google")
+
+# # close the browser window
+# driver.quit()
