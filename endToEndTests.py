b4e4a874e1f7b0051f73bbf44b1ea43e9dac3bca
Spencer Barnes <interstellarjumper@gmail.com>
2020-03-23 23:52:22 -0500

    Remove test framework dependancy

--- endToEndTests.py
+++ endToEndTests.py
@@ -3,5 +3,5 @@ from seleniumbase import BaseCase
 class HelloWorld(BaseCase):
   def test_hello_world(self):
     self.open("http://localhost:8000")
-    self.assert_title("QA-Assignment")
+    self.assert_title("QA Assignment")
     self.assert_title("Noodle")
