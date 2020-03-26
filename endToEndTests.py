d0a6bb3687555016f59640af20fa30b3cdca2ed7
Spencer Barnes <interstellarjumper@gmail.com>
2020-03-25 19:00:22 -0500

    Add onload tests

--- endToEndTests.py
+++ endToEndTests.py
@@ -2,4 +2,13 @@ from seleniumbase import BaseCase
 
 class HomePageTests(BaseCase):
   def test_page_load(self):
-    self.assert_title("QA Assignment")
+    self.assert_title('QA Assignment')
+
+class InputErrorTests(BaseCase):
+  def test_no_errors_onload(self):
+    self.assert_equal('', self.get_text('label#weightWarning'))
+    self.assert_equal('', self.get_text('label#bmiResult'))
+    
+    self.assert_equal('', self.get_text('label#salaryWarning'))
+    self.assert_equal('', self.get_text('label#saveGoalWarning'))
+    self.assert_equal('', self.get_text('label#retirementResult'))
